77faeb9b350798873de9777a258e4eee9c13dbb6
magneto903 <31848594+MAGNETO903@users.noreply.github.com>
2023-11-05 17:35:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "dalle2-laion",
     version = "0.0.1",
-    packages = ["dalle2-laion"],
+    packages = find_packages(exclude=[]),
+    include_package_data = True,
     install_requires = [
         "packaging>=21.0",
         "pydantic>=1.9.0",
         "torch>=1.10",
-        "pillow>=9.0.0",
+        "Pillow>=9.0.0",
         "numpy>=1.20.0",
         "click>=8.0.0",
         "dalle2-pytorch"
